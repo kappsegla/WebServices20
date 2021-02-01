6ef9fcb5038a2d4334376923f178ee7d1a26547e
Martin Blomberg <martin.blomberg@outlook.com>
2021-02-01 13:46:58 +0100

    ExecutorService

--- core/src/main/java/x/snowroller/ServerExample.java
+++ core/src/main/java/x/snowroller/ServerExample.java
@@ -6,10 +6,15 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServerExample {
 
     public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
         try {
             //TCP/IP
             ServerSocket serverSocket = new ServerSocket(5050);
@@ -18,8 +23,9 @@ public class ServerExample {
             while (true) {
                 Socket socket = serverSocket.accept();
 
-                Thread thread = new Thread(() -> handleConnection(socket));
-                thread.start();
+                executorService.execute(()-> handleConnection(socket));
+//                Thread thread = new Thread(() -> handleConnection(socket));
+//                thread.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -30,9 +36,19 @@ public class ServerExample {
         System.out.println(Thread.currentThread());
         try {
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            System.out.println(input.readLine());
+
+            while(true) {
+                String headerLine = input.readLine();
+                System.out.println(headerLine);
+                if( headerLine.isEmpty())
+                    break;
+            }
             var output = new PrintWriter(socket.getOutputStream());
-            output.println("Hello from Server");
+            String page = """
+                    
+                    
+                    """;
+            output.println(page);
             output.flush();
             socket.close();
         } catch (IOException e) {
