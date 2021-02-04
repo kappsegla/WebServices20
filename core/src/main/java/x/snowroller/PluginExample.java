0ae9b8111ea8a407c660df110773386564b603d3
Martin Blomberg <martin.blomberg@outlook.com>
2021-02-04 12:26:33 +0100

    Updated run instructions and pom.xml so it copies dependencies into modules folder.

--- core/src/main/java/x/snowroller/PluginExample.java
+++ core/src/main/java/x/snowroller/PluginExample.java
@@ -8,6 +8,8 @@ import java.util.ServiceLoader;
 //Run with:
 //java -p "core\target\core-1.0-SNAPSHOT.jar;spi\target\spi-1.0-SNAPSHOT.jar;plugin\target\plugin-1.0-SNAPSHOT.jar" -m core/x.snowroller.PluginExample
 
+//Running with module system, provide module path(s) and main class
+//java --module-path core-1.0-SNAPSHOT.jar;modules -m core/x.snowroller.PluginExample
 public class PluginExample {
 
     public static void main(String[] args) {
