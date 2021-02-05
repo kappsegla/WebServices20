dd9365b7b3eae339edafc8cd37128a9925ef8f34
Martin Blomberg <martin.blomberg@outlook.com>
2021-02-05 16:43:14 +0100

    Added web folder in core module. Will be copied during maven package into target folder as root for webserver files. Put new files in core/web to have them versioned in git.

--- client/src/main/java/x/snowroller/Client.java
+++ client/src/main/java/x/snowroller/Client.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 
 public class Client
 {
--- client/src/main/java/x/snowroller/ClientSocket.java
+++ client/src/main/java/x/snowroller/ClientSocket.java
@@ -9,13 +9,15 @@ public class ClientSocket {
     public static void main(String[] args) {
 
         try {
-            Socket socket = new Socket("178.174.162.51", 5050);
+            Socket socket = new Socket("localhost", 5050);
 
             var output = new PrintWriter(socket.getOutputStream());
             output.println("Hello from client");
             output.flush();
             var input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            System.out.println(input.readLine());
+            String lineFromResponse;
+            while((lineFromResponse = input.readLine()) != null)
+                System.out.println(lineFromResponse);
 
             output.close();
             socket.close();
